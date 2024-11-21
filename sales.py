e3502213e5073f0a2c9bcbad4a0b493b9c554607
LennonGarrard <s5705588@bournemouth.ac.uk>
2024-11-21 17:14:03 +0000

    Add files via upload
    Restock and Sales function both work as intended, but the 'totals do not match error' still appears

---
+++ sales.py
@@ -0,0 +1,33 @@
+import random
+
+def daily_sales(available_items, inventory_records, current_day):
+    '''
+***********COMPLETE THIS FUNCTION***********
+This function is responsible for updating the sales for a given day.
+---------------
+Function Input:
+---------------
+available_items: (integer) Available Tshirts from the previous day.
+inventory_records: (List) A list of inventory records until the previous day. Each row contains (day, sales, restocked items, available items)
+current_day: (integer) Day number which you want to add as the current day. 
+
+----------------
+Function Output:
+----------------
+available_items:(integer) This function returns this integer which updates the available items at the current day.
+
+The function will also update the inventory_records (For restocking) for a  given current day. 
+
+    '''
+    sales = 0
+    restocked_items = 0
+
+    if current_day % 7 != 0: #only on non restocking days
+        sales = random.randint(0, min(200, available_items)) #sales not exceed 200 units
+
+        available_items -= sales
+
+        inventory_records.append((current_day, sales, restocked_items, available_items))
+
+    return available_items
+    
